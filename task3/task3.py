1a4a381e76c001261e6f702a6c35855b244fa992
Vadim Pchelnikov <pchelnikovvadim797@gmail.com>
2024-02-19 20:26:32 +0400

    четвертая 3 фиксация

--- task3/task3.py
+++ task3/task3.py
@@ -15,16 +15,11 @@ def get_file_path(prompt):
     return os.path.abspath(file_path)
 
 def main():
-    # Получение текущего рабочего каталога
-    # script_path = os.path.dirname(os.path.abspath(__file__))
 
-    # Ввод путей к файлам от пользователя
     values_path = get_file_path("Введите путь к файлу values.json: ")
     tests_path = get_file_path("Введите путь к файлу tests.json: ")
     report_path = get_file_path("Введите путь к файлу report.json: ")
-    # report_path = os.path.join(script_path, 'report.json')
 
-    # Проверка существования файлов перед их загрузкой
     if not os.path.exists(values_path):
         print(f"Файл {values_path} не найден.")
         return
@@ -33,17 +28,14 @@ def main():
         print(f"Файл {tests_path} не найден.")
         return
 
-    # Загрузка данных из файлов
     with open(values_path, 'r') as values_file:
         values = json.load(values_file)
 
     with open(tests_path, 'r') as tests_file:
         tests = json.load(tests_file)
 
-    # Заполнение значений в структуре tests.json
     fill_values(tests["tests"], values)
 
-    # Запись результата в файл report.json
     with open(report_path, 'w') as report_file:
         json.dump(tests, report_file, indent=2)
 
